d956c9638fbd54f67b278f9a45b5129eb2bddf7c
Hagisf <fernando38954@gmail.com>
2024-04-16 19:02:42 -0300

    Adding Stage for Game

--- algomon/core/src/main/kotlin/com/algomon/game/Main.kt
+++ algomon/core/src/main/kotlin/com/algomon/game/Main.kt
@@ -1,6 +1,17 @@
 package com.algomon.game
 
+import com.algomon.game.screen.GameScreen
+import com.badlogic.gdx.Application
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class Main : ApplicationAdapter()
+class Main : KtxGame<KtxScreen>(){
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        addScreen(GameScreen())
+        setScreen<GameScreen>()
+    }
+}
